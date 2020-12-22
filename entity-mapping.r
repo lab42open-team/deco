77bb7c3b181b38d5e12e53d3d4fd2654575eba6c
“savvas-paragkamian” <s.paragkamian@gmail.com>
2020-12-22 20:51:44 +0200

    calculated the true positives and false negatives of gnfinder and extract

--- entity-mapping.r
+++ entity-mapping.r
@@ -6,7 +6,7 @@ library(taxize)
 library(worrms)
 
 ## Manual curation
-manual_ipt<-readxl::read_excel("occurrence_Forbes_ipt.xls")
+manual_ipt<-readxl::read_excel("occurrence_Forbes_ipt.xls") %>% select(scientificName, fieldNumber, catalogNumber, occurrenceID) %>% distinct()
 
 ## NER
 
@@ -16,8 +16,14 @@ extract_organisms <- extract_associations %>% filter(`entity type`==-2) %>% dist
 
 gnfinder_species <- jsonlite::read_json("gnfinder-forbes.json",simplifyVector = TRUE)
 
+gnfinder_species_vector <- unique(gnfinder_species[[2]][3])
+
+
+
 ## Entity Mapping
 #get_wormsid
 
+# Precision - Recall
 
-## 
+roc <- manual_ipt %>% mutate(gnfinder=(scientificName %in%gnfinder_species_vector$name)) %>% mutate(extract_species=(scientificName %in% extract_organisms$`tagged text`))
+
