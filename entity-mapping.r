efaeff8ba40861dbbecb34dca56f56c6700e4279
“savvas-paragkamian” <s.paragkamian@gmail.com>
2020-12-28 11:31:03 +0200

    added word boundaries to filter the species

--- entity-mapping.r
+++ entity-mapping.r
@@ -14,16 +14,19 @@ extract_associations <- read_delim("extract-forbes.tsv",delim="\t")
 
 extract_organisms <- extract_associations %>% filter(`entity type`==-2) %>% distinct()
 
-gnfinder_species <- jsonlite::read_json("gnfinder-forbes.json",simplifyVector = TRUE)
+gnfinder_organisms <- jsonlite::read_json("gnfinder-forbes.json",simplifyVector = TRUE)
 
-gnfinder_species_vector <- unique(gnfinder_species[[2]][3])
+gnfinder_vector <- unique(gnfinder_organisms[[2]][3])
 
 
+gnfinder_species_vector <- gnfinder_vector %>% filter(grepl(x=name, pattern='\\w \\w'))
+
+
+manual_ipt_species <- manual_ipt %>% filter(grepl(x=scientificName, pattern='\\w \\w'))
 
 ## Entity Mapping
 #get_wormsid
 
 # Precision - Recall
 
-roc <- manual_ipt %>% mutate(gnfinder=(scientificName %in%gnfinder_species_vector$name)) %>% mutate(extract_species=(scientificName %in% extract_organisms$`tagged text`))
-
+roc <- manual_ipt_species %>% mutate(gnfinder=(scientificName %in%gnfinder_vector$name)) %>% mutate(extract_species=(scientificName %in% extract_organisms$`tagged text`))
