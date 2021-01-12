78c2083939a9ec39db465b7f74a5bd4975deb3f9
“savvas-paragkamian” <s.paragkamian@gmail.com>
2021-01-12 20:16:36 +0200

    ocr is ready

--- scripts/cli-workflow.sh
+++ scripts/cli-workflow.sh
@@ -5,13 +5,13 @@
 ## User input PDF file
 
 ## Usage of the script
-usage="Use the parameter -f for the pdf file (full name) and -d for the name of the new directory that the results will be saved in. \n"
+usage="Use the parameter -f for the path of pdf file (full path name) and -d for the name of the new directory that the results will be saved in. \n"
 
 while getopts "f:d:" option
 do
    case "$option" in
-      f)   PDF_FILE="${OPTARG}";;
-      d)   DIRECTORY="${OPTARG}";;
+      f)   pdf_file="${OPTARG}";;
+      d)   directory="${OPTARG}";;
       ?|:)   echo -e "$usage" ; exit 1;;
       *)   echo -e "option ${OPTARG} unknown. \n$usage" ; exit 1 ;;
    esac
@@ -24,25 +24,55 @@ if [ $OPTIND -eq 1 ];
 fi
 
 ## Detect if a single option is missing
-if [ -z "${PDF_FILE}" ]; then
+if [ -z "${pdf_file}" ]; then
     echo -e "Option -f empty. $usage"; exit 1;
 fi
 
-if [ -z "${DIRECTORY}" ]; then
+if [ -z "${directory}" ]; then
     echo -e "Option -d empty. $usage"; exit 1;
 fi
 
 ## Successful call of the script parameters
-echo "The file: $PDF_FILE will be processed for species and environments and the output will be saved in the directory: $DIRECTORY"
+echo -e "data: $pdf_file \noutput directory: $directory \n"
 
-## OCR
-#convert -density 400 legacy-literature.pdf -quality 100 legacy-literature.png
-#
-#for f in *.png; do tesseract -l eng $f ${f%".png"}; done
-#
-#cat *.txt >> legacy-literature.txt
-#
+## Creation of new directory for the outputs
+
+time_start=`date +%s`
+DATE=$(date +"%Y-%m-%d")
+
+### assign a unique random tag to files.
+id=$RANDOM
+
+script_path="${BASH_SOURCE[0]}"
+cd `dirname "$script_path"`
+echo `dirname "$script_path"`
+
+cd ../
+mkdir -p $directory;
+cd $directory
+
+text_output=ocr-${id}.txt
+touch $text_output
+
+
+## From pdf to text
+
+### break into single pages of images with ImageMagick
+echo -e "conversion started \n"
+convert -density 400 $pdf_file -quality 100 ${id}.png
+
+### from images to text files
+
+echo -e "OCR with tesseract started \n"
+for f in ${id}*.png; do tesseract -l eng $f ${f%".png"}; done
+
+### combine all texts to one
+
+cat ${id}*.txt > $text_output
+
+echo -e "text from $pdf_file is in $directory/$text_output \n"
 ### NER
+
 #### EXTRACT
 #
 #./getEntities_EXTRACT_api.pl legacy-literature.txt > legacy-literature-extract.tsv
