cddfea9ddeb762ed8ed3f7876c8cbf7e8f7fb3ff
“savvas-paragkamian” <s.paragkamian@gmail.com>
2021-01-18 15:27:12 +0200

    Worms API for NCBI ids is ready

--- scripts/entity_mapping.r
+++ scripts/entity_mapping.r
@@ -2,9 +2,8 @@
 
 ## user arguments
 
-## Packages
+# Packages
 library(tidyverse)
-library(taxize)
 library(worrms)
 library(httr)
 
@@ -19,13 +18,63 @@ worms_api <- function(ncbi_id){
     url <- paste("http://www.marinespecies.org/rest/AphiaRecordByExternalID/",ncbi_id,"?type=ncbi",sep="")
     get_url <- GET(url)
     message_for_status(get_url)
+    worms_status <- status_code(get_url)
+    worms_content <- content(get_url)
+    
+    if (worms_status==200){
 
+        return(worms_content)
+    } else {
+
+        return(worms_status)
+    }
 }
 
+list_null <- function(x) {
+
+    if (is.null(x)){
+        return(NA)
+    } else {
+        return(x)
+    }
+}
+
+worms_content <- list()
+
+worms_df <- data.frame(matrix(data = NA,nrow= nrow(extract_org),ncol=28))
+
+colnames(worms_df) <- c("AphiaID","url","scientificname","authority","status","unacceptreason","taxonRankID","rank","valid_AphiaID","valid_name","valid_authority","parentNameUsageID","kingdom","phylum","class","order","family","genus","citation","lsid","isMarine","isBrackish","isFreshwater","isTerrestrial","isExtinct","match_type","modified","ncbi")
+
+for (i in seq(1,nrow(extract_org),by=1)){
+    
+    ncbi=as.character(extract_org[i,4])
+    worms_df$ncbi[i] <- ncbi
+    
+    worms_content <- worms_api(ncbi)
+    
+    if (!is.numeric(worms_content)) {
+             worms_content <- lapply(worms_content, list_null)
+    }
+
+    for (j in seq(1,27,by=1)) {
+        print(j)
+
+        if (!is.numeric(worms_content)) {
+            
+            worms_df[i,j] <- worms_content[[j]]
+
+        } else {
+
+            worms_df[i,j] <- worms_content
+        }
+    }
+
+    Sys.sleep(0.5)
+}
 
 ## get worms id
 
-worms_id <- as.data.frame(get_wormsid(as.vector(gnfinder$X1),fuzzy=T, rows=1))
+#worms_id <- as.data.frame(get_wormsid(as.vector(gnfinder$X1),fuzzy=T, rows=1))
 
 #for i in `cat file`; do curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=taxonomy&id=$i&rettype=docsum&retmode=text" | head -1 | sed -e 's/1. //g' | awk -F "\t" '{print '$i'"\t"$0}'; done;
 #
