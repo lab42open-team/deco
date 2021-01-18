299393ebaa26718f9a426a64a737bc75f39afadc
“savvas-paragkamian” <s.paragkamian@gmail.com>
2021-01-19 01:16:01 +0200

    error in empty values

--- scripts/entity_mapping.r
+++ scripts/entity_mapping.r
@@ -10,26 +10,21 @@ library(httr)
 source('functions_entity_mapping.r')
 
 ## Input
+rms_content)
 gnfinder <- read_delim("../output/30815-gnfinder-species.tsv", delim="\t",col_names=F)
 extract_file <- read_delim("../output/30815-extract.tsv", delim="\t",col_names=T) %>% filter(entity_type==-2) %>% mutate(ncbi_id=gsub('NCBI:','',term_id))
 
 
-extract_aphia_ids <- get_AphiaIDs("EXTRACT",head(extract_file$ncbi_id))
-
 # Output of NCBI to worms id
+extract_aphia_ids <- get_AphiaIDs_extract(head(extract_file$ncbi_id))
 
-write_delim(worms_df, "../output/extract_organisms_worms.tsv", delim="\t", col_names=T)
+write_delim(extract_aphia_ids, "../output/extract_organisms_worms.tsv", delim="\t", col_names=T)
 
-## get worms id from scientific names
+## Output of worms id from scientific names
 
 gnfinder_names_url <- gsub(gnfinder$X1, pattern=" ", replacement="%20")
 
+gnfinder_aphia_ids <- get_AphiaIDs_gnfinder(gnfinder_names_url)
 
 
-
-#worms_id <- as.data.frame(get_wormsid(as.vector(gnfinder$X1),fuzzy=T, rows=1))
-
-#for i in `cat file`; do curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=taxonomy&id=$i&rettype=docsum&retmode=text" | head -1 | sed -e 's/1. //g' | awk -F "\t" '{print '$i'"\t"$0}'; done;
-#
-
-
+write_delim(gnfinder_aphia_ids, "../output/gnfinder_organisms_worms.tsv", delim="\t", col_names=T)
