594543bced8abff31ec2a1c3d312044ee981afc0
“savvas-paragkamian” <s.paragkamian@gmail.com>
2021-01-19 11:41:28 +0200

    fixed path issues in the output

--- scripts/entity_mapping.r
+++ scripts/entity_mapping.r
@@ -24,18 +24,18 @@ random_id <- args[1]
 library(tidyverse)
 library(httr)
 
-# Load Custom Functions
-source('functions_entity_mapping.r')
+# Load Custom Functions. Between each call of the api 0.5 second interval is set not to overload the servers of WORMS.
+source("scripts/functions_entity_mapping.r")
 
 ## Input
-extract_file <- read_delim(paste("../output/",random_id,"extract.tsv",sep=""), delim="\t",col_names=T) %>% filter(entity_type==-2) %>% mutate(ncbi_id=gsub('NCBI:','',term_id))
+extract_file <- read_delim(paste("output/",random_id,"-extract.tsv",sep=""), delim="\t",col_names=T) %>% filter(entity_type==-2) %>% mutate(ncbi_id=gsub('NCBI:','',term_id))
 
-gnfinder <- read_delim(paste("../output/",random_id,"-gnfinder-species.tsv",sep=""), delim="\t",col_names=F)
+gnfinder <- read_delim(paste("output/",random_id,"-gnfinder-species.tsv",sep=""), delim="\t",col_names=F)
 
 ## Output of NCBI to worms id
 extract_aphia_ids <- get_AphiaIDs_extract(head(extract_file$ncbi_id))
 
-write_delim(extract_aphia_ids, paste("../output/",random_id,"extract_organisms_worms.tsv",sep=""), delim="\t", col_names=T)
+write_delim(extract_aphia_ids, paste("output/",random_id,"-extract_organisms_worms.tsv",sep=""), delim="\t", col_names=T)
 
 ## Output of worms id from scientific names
 
@@ -43,4 +43,4 @@ gnfinder_names_url <- gsub(gnfinder$X1, pattern=" ", replacement="%20")
 
 gnfinder_aphia_ids <- get_AphiaIDs_gnfinder(gnfinder_names_url)
 
-write_delim(gnfinder_aphia_ids, paste("../output/",random_id,"gnfinder_organisms_worms.tsv", sep=""), delim="\t", col_names=T)
+write_delim(gnfinder_aphia_ids, paste("output/",random_id,"-gnfinder_organisms_worms.tsv", sep=""), delim="\t", col_names=T)
